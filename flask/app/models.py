d291ee1680f99eebed6e47318e0a6baaa105baf1
Trevor Sullivan <Trevor.Sullivan@ayfie.com>
2018-08-20 17:04:46 -0400

    Fixed query url, added db columns, all done except one I think.

--- flask/app/models.py
+++ flask/app/models.py
@@ -15,14 +15,21 @@ class User(db.Model):
     channel = db.Column(db.String(32), index= True, unique=True)
     user_name = db.Column(db.String(64), index=True)
     awaiting_response = db.column(db.Boolean())
+    conversation = None # TODO list of issues
 
     def __repr__(self):
         return "<User {}>".format(self.user_name)
 
-# class Issues(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     team = db.Column(db.String(64), index=True)
-#     key = db.Column(db.String(64), index=True)
-#http://flask-sqlalchemy.pocoo.org/2.3/models/#many-to-many-relationships
-#https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
-#http://ondras.zarovi.cz/sql/demo/
+class Issues(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    team = db.Column(db.String(64), index=True)
+    key = db.Column(db.String(64), index=True)
+    summary = db.Column(db.String(256))
+    url = db.Column(db.String(128))
+
+    def __repr__(self):
+        return "<{} Issue {}>".format(self.team, self.key)
+
+# http://flask-sqlalchemy.pocoo.org/2.3/models/#many-to-many-relationships
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+# http://ondras.zarovi.cz/sql/demo/
